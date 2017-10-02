228f8d1817525c0b5741f7c80db40a1cfe1977ca
Max Wang <maxwellswang@gmail.com>
2017-10-02 16:15:02 -0400

    fixed bug to show giphs of specific button clicked

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,21 @@
 var buttons = ["Michael Scott", "Dwight Shrute", "Jim Halpert", "Pam Beasley", "Andy Bernard", "Angela Martin", "Toby Flenderson", "Creed Bratton"];
-var queryURL = "http://api.giphy.com/v1/gifs/search?&api_key=dc6zaTOxFJmzC&limit=10&q=michael+scott";
+var queryURL = "http://api.giphy.com/v1/gifs/search?&api_key=dc6zaTOxFJmzC&limit=10";
 
-$("#buttons").on("click", function() {
+
+for (var i = 0; i < buttons.length; i++) {
+	$("#buttons").append("<button>" + buttons[i] + "</button>");
+};
+
+$("button").on("click", function() {
+	queryURL += "&q=" + $(this).text();
+	console.log(queryURL)
 	$.ajax({
 		url: queryURL,
 		method: "GET",
 	}).done(function(response) {
 		for (var i = 0; i < 10; i++) {
 			var result = response.data[i].images.fixed_height.url;
-			console.log(result);
 			$("#giphs").prepend("<img src=" + result + ">");
 		}
 	})
 });
-
-for (var i = 0; i < buttons.length; i++) {
-	$("#buttons").append("<button>" + buttons[i] + "</button>");
-};
