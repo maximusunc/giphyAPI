08fae43f2e423f6f5cb1b3197439a1d18e32e4c1
Max Wang <maxwellswang@gmail.com>
2017-10-02 15:52:45 -0400

    added working ajax function for api

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,18 @@
 var buttons = ["Michael Scott", "Dwight Shrute", "Jim Halpert", "Pam Beasley", "Andy Bernard", "Angela Martin", "Toby Flenderson", "Creed Bratton"];
+var queryURL = "http://api.giphy.com/v1/gifs/search?&api_key=dc6zaTOxFJmzC&limit=10&q=michael+scott";
+
+$("#buttons").on("click", function() {
+	$.ajax({
+		url: queryURL,
+		method: "GET",
+	}).done(function(response) {
+		for (var i = 0; i < 10; i++) {
+			var result = response.data[i].images.fixed_height.url;
+			console.log(result);
+			$("#giphs").prepend("<img src=" + result + ">");
+		}
+	})
+});
 
 for (var i = 0; i < buttons.length; i++) {
 	$("#buttons").append("<button>" + buttons[i] + "</button>");
