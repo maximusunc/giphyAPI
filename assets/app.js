5de4e24e8186eececc65f53814a2ab01840fb68d
Max Wang <maxwellswang@gmail.com>
2017-10-02 15:23:04 -0400

    initial file creation and foundation of html

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+var buttons = ["Michael Scott", "Dwight Shrute", "Jim Halpert", "Pam Beasley", "Andy Bernard", "Angela Martin", "Toby Flenderson", "Creed Bratton"];
+
+for (var i = 0; i < buttons.length; i++) {
+	$("#buttons").append("<button>" + buttons[i] + "</button>");
+};
