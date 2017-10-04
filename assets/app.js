593da8e876ebf006eb8d4a442d1b938f2ad3dff8
Max Wang <maxwellswang@gmail.com>
2017-10-04 13:55:22 -0400

    gifs switch between still and animated, though style is messed up

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,29 @@
-var buttons = ["Michael Scott", "Dwight Shrute", "Jim Halpert", "Pam Beasley", "Andy Bernard", "Angela Martin", "Toby Flenderson", "Creed Bratton"];
-var queryURL = "http://api.giphy.com/v1/gifs/search?&api_key=dc6zaTOxFJmzC&limit=10";
+var topics = ["Michael Scott", "Dwight Shrute", "Jim Halpert", "Pam Beasley", "Andy Bernard", "Angela Martin", "Toby Flenderson", "Creed Bratton"];
+var queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=dc6zaTOxFJmzC&limit=10";
+var still;
+var animated;
+var rating;
 
 
-for (var i = 0; i < buttons.length; i++) {
-	$("#buttons").append("<button>" + buttons[i] + "</button>");
+function displayButtons () {
+	for (var i = 0; i < topics.length; i++) {
+		$("#buttons").append("<button>" + topics[i] + "</button>");
+	};
 };
 
-$("button").on("click", function() {
+$("#addButton").on("click", function () {
+	event.preventDefault();
+	if ($("#addGiph").val() !== "") {
+		topics.push($("#addGiph").val().trim());
+		$("#buttons").empty();
+		$("#addGiph").val("");
+		displayButtons();
+	};
+});
+
+displayButtons();
+
+$("#buttons").on("click", "button", function() {
 	queryURL += "&q=" + $(this).text();
 	console.log(queryURL)
 	$.ajax({
@@ -14,8 +31,24 @@ $("button").on("click", function() {
 		method: "GET",
 	}).done(function(response) {
 		for (var i = 0; i < 10; i++) {
-			var result = response.data[i].images.fixed_height.url;
-			$("#giphs").prepend("<div><img src=" + result + ">" + "<br><br>" + "Rating: " + response.data[i].rating + "</div>");
+			still = response.data[i].images.fixed_height_still.url;
+			animated = response.data[i].images.fixed_height.url;
+			rating = response.data[i].rating;
+			var giph = "<img src=" + still + " data-still=" + still + " data-animated=" + animated + " data-state=still class=giph>" + "<br><br>" + "Rating: " + rating;
+			$("#giphs").prepend(giph);
 		}
+		$(".giph").on("click", function() {
+			var state = $(this).attr("data-state");
+			if (state === "still") {
+				$(this).attr("src", $(this).attr("data-animated"));
+				$(this).attr("data-state", "animated");
+				console.log($(this).attr("src"));
+			} else {
+				$(this).attr("src", $(this).attr("data-still"));
+				$(this).attr("data-state", "still");
+				console.log($(this).attr("src"));
+			}
+		});
 	})
 });
+
